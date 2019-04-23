0fa924cb0932a167d3c576ee77264f77642337f7
Soraya Saenna <bubblebitoey@gmail.com>
2019-04-23 11:27:05 +0700

    remove bus

--- public/data.js
+++ public/data.js
@@ -2,9 +2,8 @@ var array = [
   JSON.stringify(credit),
   JSON.stringify(disability),
   JSON.stringify(motorbike),
-  JSON.stringify(bus),
   JSON.stringify(at),
   JSON.stringify(lateMorning),
   JSON.stringify(car),
-  JSON.stringify(lost)
+  JSON.stringify(lost),
 ]
